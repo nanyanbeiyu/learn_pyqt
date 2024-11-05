ccb4ee57fb5a24f5febb4d01761db7d306ba6517
GGBO <2767840563@qq.com>
2024-11-05 08:42:18 +0800

    体验窗口的一些使用方法

--- Code/01_创建窗口.py
+++ Code/01_创建窗口.py
@@ -7,6 +7,19 @@ app = QApplication(sys.argv)
 # 2.创建窗口
 w = QWidget()
 
+# 2.1设置窗口标题
+w.setWindowTitle("Hello PyQt5")
+
+# 2.2设置窗口大小
+
+w.resize(1280,720)
+
+#2.3 指定窗口的位置
+# w.move(400,100)
+
+#2.4 指定窗口位置和大小
+w.setGeometry(400,100,1280,720)
+
 # 3.显示窗口
 w.show()
 
